274a6537f48eb3f061e02b92f380d9685ef82821
jynti <33417806+jynti@users.noreply.github.com>
2018-01-02 14:29:56 +0530

    Update footer.js

--- productSorting/js/footer.js
+++ productSorting/js/footer.js
@@ -1,41 +1,50 @@
-function Footer(visibleProducts, selectedPagination, domElements){
+function Footer(visibleProducts, selectedPagination, domElements) {
   this.visibleProducts = visibleProducts;
   this.selectedPagination = selectedPagination;
   this.footer = domElements.footer;
   this.productContentArea = domElements.productContentArea;
+  this.url = domElements.url;
+  this.dropdown = domElements.dropdown;
 }
 
-Footer.prototype.init = function(){
+Footer.prototype.init = function() {
   this.numberOfButtonsToCreate();
   this.createButtons();
 }
 
-Footer.prototype.numberOfButtonsToCreate = function(){
-  this.numberOfButtonsToCreate = Math.ceil(this.visibleProducts.length/this.selectedPagination);
+Footer.prototype.numberOfButtonsToCreate = function() {
+  this.numberOfButtonsToCreate = Math.ceil(this.visibleProducts.length / this.selectedPagination);
 }
 
-Footer.prototype.createButtons = function(){
-  var _this = this;
+Footer.prototype.createButtons = function() {
   this.footer.empty();
-  for(var i = 0; i < this.numberOfButtonsToCreate; i++){
-    var pageNumber = $("<span></span>").text(i+1).addClass("page-number");
-    (function(buttonNumber){
-      pageNumber.on("click", function(){
-        _this.onButtonClick(buttonNumber);
-      });
-    })(i);
-    this.footer.append(pageNumber);
+  var buttons = [];
+  for (var buttonNumber = 0; buttonNumber < this.numberOfButtonsToCreate; buttonNumber++) {
+    var pageNumber = $("<span></span>").text(buttonNumber + 1).addClass("page-number").data("button-number", buttonNumber);
+    buttons.push(pageNumber);
   }
+  this.footer.append(buttons);
+  this.onButtonClick();
 }
 
-Footer.prototype.onButtonClick = function(buttonNumber){
-  var start = buttonNumber  * (+this.selectedPagination);
-  var end = start + (+this.selectedPagination) - 1;
+Footer.prototype.onButtonClick = function() {
+  var _this = this;
+  $(".page-number").on("click", function() {
+    var buttonNumber = $(this).data("button-number");
+    _this.url["pageNumber"] = buttonNumber;
+    window.location.hash = JSON.stringify(_this.url);
+    _this.display(buttonNumber);
+  });
+}
+
+Footer.prototype.display = function(buttonNumber) {
+  var fromProduct = buttonNumber * (+this.selectedPagination);
+  var tillProduct = fromProduct + (+this.selectedPagination) - 1;
   this.highlightButton(buttonNumber);
-  Product.show(start, end, this.visibleProducts, this.productContentArea);
+  Product.show(fromProduct, tillProduct, this.visibleProducts, this.productContentArea, this.dropdown);
 }
 
 
-Footer.prototype.highlightButton = function(buttonNumber){
+Footer.prototype.highlightButton = function(buttonNumber) {
   this.footer.find("span").eq(buttonNumber).css("color", "red").siblings().css("color", "white");
 }
